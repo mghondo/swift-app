6fe037f694f30fa5ea9532f551841fe0e78421a7
Morgan Hondros <morganhondros@gmail.com>
2018-12-27 13:07:42 -0500

    My first commit.

---
+++ Swift App/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  Swift App
+//
+//  Created by Morgan Hondros on 12/27/18.
+//  Copyright © 2018 Morgan Hondros. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var coolLabel: UILabel!
+    
+    var tapCount = 0
+    
+    @IBAction func buttonTapped(_ sender: Any) {
+        tapCount = tapCount + 1
+        
+        if tapCount >= 10 {
+            coolLabel.text = "You tapped the button 10 times!"
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+
+
+}
+
